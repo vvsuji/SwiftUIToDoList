e74152668986fe7ca342f0f57cf0142b4e79b5a4
vvsuji <suji.falcon@gmail.com>
2024-05-25 08:41:25 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Suji Falcon on 5/25/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
