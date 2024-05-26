0ac54d93291780b5ada21247757de4447f03de26
vvsuji <suji.falcon@gmail.com>
2024-05-26 13:51:28 -0400

    signout functionality complete

--- ToDoList/Views/ProfileView.swift
+++ ToDoList/Views/ProfileView.swift
@@ -13,48 +13,59 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack {
-                // Avatar
-                Image(systemName: "person.circle")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .foregroundStyle(Color.purple)
-                    .frame(width: 125, height: 125)
-                    .padding()
-                
-                // Info: Name, Email, Member since
-                VStack(alignment: .leading) {
-                    HStack {
-                        Text("Name: ")
-                            .bold()
-                        Text("Suji Falcon")
-                    }
-                    .padding()
-                    
-                    HStack {
-                        Text("Email: ")
-                            .bold()
-                        Text("suji@vvsuji.dev")
-                    }
-                    .padding()
-                    
-                    HStack {
-                        Text("Member since: ")
-                            .bold()
-                        Text("5/25/2024")
-                    }
-                    .padding()
+                if let user = viewModel.user {
+                    profile(user: user)
+                } else {
+                    Text("Loading Profile...")
                 }
-                
-                // Sign out
-                Button("Sign Out") {
-                    viewModel.logOut()
-                }
-                .tint(.purple)
-                .padding()
-                Spacer()
             }
             .navigationTitle("Profile")
         }
+        .onAppear() {
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundStyle(Color.purple)
+            .frame(width: 125, height: 125)
+            .padding()
+        
+        // Info: Name, Email, Member since
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            
+            HStack {
+                Text("Member since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        
+        // Sign out
+        Button("Sign Out") {
+            viewModel.logOut()
+        }
+        .tint(.purple)
+        .padding()
+        Spacer()
     }
 }
 
